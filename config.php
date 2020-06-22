8e4ce6794260f6b98ab776d687eccf0e4261d33f
Giang <trgiang1998@gmail.com>
2020-06-22 22:38:58 +0700

    commit working php demo

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$vnp_TmnCode = "0O7E257P"; //website code
+$vnp_HashSecret = "9VJCS2UXDZJOLYEQENMUTU1EXI893HV7"; //secret key
+$vnp_Url = "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+$vnp_Returnurl = "http://localhost/vnpay_php/vnpay_return.php";
